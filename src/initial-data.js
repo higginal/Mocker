091c52845bb0f70b253eb2e6036c86a6a11f3287
higginal <higginal@dukes.jmu.edu>
2020-02-17 01:54:24 -0500

    all new changes

--- src/initial-data.js
+++ src/initial-data.js
@@ -1,18 +1,28 @@
+import players from './players.js'
+
+
+var request = new XMLHttpRequest();
+request.open("GET", "./data/players.json", false);
+request.send(null)
+
 const initialData = {
 
     'teams': {
         'team-1': { id: 'team-1', content: 1 }
     },
 
-    'players' : {
+    'players': players,
+
+
+    /*{
 
         'Joe-Burrow': { id: 'Joe-Burrow', content: 'Joe Burrow', pos: 'Quarterback', school: 'LSU' },
         'Chase-Young': { id: 'Chase-Young', content: 'Chase Young', pos: 'Edge', school: 'OSU' },
         'Tua-T': { id: 'Tua-T', content: 'Tua T', pos: 'Quarterback', school: 'Alabama' }
 
-    },
+    }, */
 
-    'allPlayerIds' : ['Joe-Burrow', 'Chase-Young', 'Tua-T'],
+    'allPlayerIds': ['Joe-Burrow', 'Chase-Young', 'Tua-T'],
 
     columns: {
         'team-column': {
@@ -34,7 +44,7 @@ const initialData = {
         }
     },
 
-    columnOrder : ['team-column', 'player-column', 'player-pool']
+    columnOrder: ['team-column', 'player-column', 'player-pool']
 
 }
 
---
+++ src/players.js
@@ -0,0 +1,7 @@
+export default players = {
+
+    'Joe-Burrow': { id: 'Joe-Burrow', content: 'Joe Burrow', pos: 'Quarterback', school: 'LSU' },
+    'Chase-Young': { id: 'Chase-Young', content: 'Chase Young', pos: 'Edge', school: 'OSU' },
+    'Tua-T': { id: 'Tua-T', content: 'Tua T', pos: 'Quarterback', school: 'Alabama' }
+
+}
