6024406c3dfa741ebfd2d2eb7d565e4e94904e6b
higginal <higginal@dukes.jmu.edu>
2020-02-20 10:22:07 -0500

    oh dear

---
+++ build/precache-manifest.0a54892292f5926be245c381700c2852.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e280a88f39027165bb58d97758731f9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68ddab18f4d68dfc988a",
+    "url": "/static/css/2.4121280a.chunk.css"
+  },
+  {
+    "revision": "a739cd99a6f72fa4d26b",
+    "url": "/static/css/main.1f2121dd.chunk.css"
+  },
+  {
+    "revision": "68ddab18f4d68dfc988a",
+    "url": "/static/js/2.0b46e030.chunk.js"
+  },
+  {
+    "revision": "bf35789e96cee4281827707d8cfc8224",
+    "url": "/static/js/2.0b46e030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a739cd99a6f72fa4d26b",
+    "url": "/static/js/main.b0c60c16.chunk.js"
+  },
+  {
+    "revision": "574c01249b36c86c9a0f",
+    "url": "/static/js/runtime-main.238e0f67.js"
+  },
+  {
+    "revision": "58018991b2ad5823930471bf2ed14848",
+    "url": "/static/media/yellow_brown_logo.58018991.png"
+  }
+]);
