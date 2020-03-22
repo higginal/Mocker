895c58c962d976118dbb9a79140aad165d2dc59b
higginal <higginal@dukes.jmu.edu>
2020-03-22 13:27:02 -0400

    added axios and url slot

--- src/SaveModal.js
+++ src/SaveModal.js
@@ -5,17 +5,40 @@ import Form from 'react-bootstrap/Form';
 import DraftCompiler from './DraftCompiler'
 import styled from 'styled-components';
 
-const TinyTextArea = styled(Form.Control)`
+const axios = require('axios');
+
+const TinyTextArea = styled(Form.Control) `
     font-size: 5pt;
     width: 100%;
 
 `
 
+const LinkLocation = styled.div`
+    display: flex;
+    margin-top: 5px;
+    width: 100%;
+    height: 65px;
+    
+
+`
+
+
+const UrlTextArea = styled(Form.Control) `
+    
+    width: 100%;
+    margin-left: 5px;
+    resize: none;
+    max-height: 85px;
+
+`
+
+const mySecret = "a4f7a4da6d24ed133719fedd21bcb42b";
+
 function createBinary(stateData) {
 
     let objJsonStr = JSON.stringify(stateData);
     let objJsonB64 = Buffer.from(objJsonStr).toString("base64");
-        
+
     //console.log(objJsonB64);
 
     //var actual = JSON.parse(atob(objJsonB64));
@@ -31,11 +54,13 @@ function createBinary(stateData) {
 
 
 
-export default function ShareModal(props)  {
+export default function ShareModal(props) {
 
 
     let testText = createBinary(props.allData);
+    let testPBUrl = "pastebin.filler.com";
     var textArea;
+    var urlArea;
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -49,6 +74,25 @@ export default function ShareModal(props)  {
         textArea.select();
         document.execCommand('copy');
     }
+    const postLink = () => {
+
+        axios.post('https://pastebin.com/api/api_post.php', {
+            api_dev_key: mySecret,
+            api_option: "paste",
+            api_paste_code: testText
+          })
+          .then(function (response) {
+            testPBUrl = response;
+            console.log(response);
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+
+
+        
+        urlArea.value = testPBUrl;
+    }
 
     return (
         <>
@@ -56,10 +100,16 @@ export default function ShareModal(props)  {
         <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton><Modal.Title>Shareable Data:</Modal.Title></Modal.Header>
             <Modal.Body>
-                
-                <TinyTextArea as="textarea" rows="50" ref={(textarea) => textArea = textarea} defaultValue={testText}>
+
+                <TinyTextArea as="textarea" rows="40" ref={(textarea) => textArea = textarea} defaultValue={testText}>
                 </TinyTextArea>
-                <Button onClick={copyToClipboard}>Copy</Button>
+                <Button onClick={copyToClipboard}>Copy</Button> <br />
+                <LinkLocation id="pastebin">
+                    <Button onClick={postLink}>Save to Pastebin</Button>
+                    <UrlTextArea as="textarea" rows="1" ref={(url) => urlArea = url}>
+
+                    </UrlTextArea>
+                </LinkLocation>
             </Modal.Body>
         </Modal>
         </>
