1e51dad9469cff01e47adabc27b84c85d1014380
higginal <higginal@dukes.jmu.edu>
2020-03-21 23:57:26 -0400

    added copy button and output

--- src/SaveModal.js
+++ src/SaveModal.js
@@ -11,10 +11,31 @@ const TinyTextArea = styled(Form.Control)`
 
 `
 
+function createBinary(stateData) {
+
+    let objJsonStr = JSON.stringify(stateData);
+    let objJsonB64 = Buffer.from(objJsonStr).toString("base64");
+        
+    //console.log(objJsonB64);
+
+    //var actual = JSON.parse(atob(objJsonB64));
+    //console.log(actual);
+    return objJsonB64;
+
+
+    //var testConvert = btoa(draftInfo);
+    //return testConvert;
+
+}
+
+
+
+
 export default function ShareModal(props)  {
 
 
-    let testText = "Lorem Ipsum";
+    let testText = createBinary(props.allData);
+    var textArea;
 
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
@@ -24,6 +45,11 @@ export default function ShareModal(props)  {
 
     }
 
+    const copyToClipboard = () => {
+        textArea.select();
+        document.execCommand('copy');
+    }
+
     return (
         <>
         <Button onClick={handleShow} variant="danger">Save</Button>
@@ -31,9 +57,9 @@ export default function ShareModal(props)  {
             <Modal.Header closeButton><Modal.Title>Shareable Data:</Modal.Title></Modal.Header>
             <Modal.Body>
                 
-                <TinyTextArea as="textarea" rows="50" defaultValue={testText}>
-
+                <TinyTextArea as="textarea" rows="50" ref={(textarea) => textArea = textarea} defaultValue={testText}>
                 </TinyTextArea>
+                <Button onClick={copyToClipboard}>Copy</Button>
             </Modal.Body>
         </Modal>
         </>
